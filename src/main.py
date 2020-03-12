d35e2e18f62d176ad5ff75b3b09149e92e818a61
George Joseph <gjnet@live.com>
2020-03-12 17:14:01 +0000

    autokeras implementation

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 #%%
 
-import pandas as pd
+import gc
+import os
+
 import numpy as np
+import pandas as pd
 # import vaex
 # from IPython.display import display
 # from vaex.ml.sklearn import Predictor
 import xgboost as xgb
-import autokeras as ak
 from sklearn import model_selection as skl_ms
-import gc
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['MAX_BATCH_SIZE'] = '32'
+import autokeras as ak
 
 
 #%%
@@ -16,10 +21,10 @@ import gc
 df_pd = pd.DataFrame(np.random.randint(2, size=(10**7, 100), dtype=np.uint8))
 gc.collect()
 
-df_pd.columns = [str(vname) for vname in list(df_pd.columns.values)]
+df_pd.columns = [str(vname) for vname in list(df_pd.columns)]
 target_vname = "target"
 df_pd.rename(columns={"99": target_vname}, inplace=True, errors="ignore")
-features = [vname for vname in list(df_pd.columns.values) if vname != target_vname]
+features = [vname for vname in list(df_pd.columns) if vname != target_vname]
 df_pd_train, df_pd_test = skl_ms.train_test_split(df_pd, test_size=0.3, random_state=123)
 X_train, y_train = df_pd_train[features], df_pd_train[target_vname]
 X_test, y_test = df_pd_test[features], df_pd_test[target_vname]
@@ -31,10 +36,13 @@ date_time_fmt = "%Y/%m/%d %H:%M:%S"
 start_time = pd.to_datetime("now")
 print("Start time:", start_time.strftime(date_time_fmt))
 
-xgb_clf = xgb.XGBClassifier(objective="binary:logistic", tree_method="approx", eval_metric="logloss")
-xgb_clf.fit(X_train, y_train, )
-xgb_pred_y = xgb_clf.predict(X_test)
-print(xgb_clf.evaluate(X_test, y_test))
+xgb_clf = xgb.XGBClassifier(tree_method="approx", ntree_limit=10)
+xgb_clf.fit(X_train, y_train, eval_set=[(X_test, y_test)], eval_metric="logloss", early_stopping_rounds=1)
+xgb_evals_result = xgb_clf.evals_result()
+print(xgb_evals_result)
+
+del xgb_clf
+gc.collect()
 
 finish_time = pd.to_datetime("now")
 print("Finish time:", finish_time.strftime(date_time_fmt), "or", (finish_time-start_time).seconds//60, "minutes.")
@@ -44,9 +52,8 @@ date_time_fmt = "%Y/%m/%d %H:%M:%S"
 start_time = pd.to_datetime("now")
 print("Start time:", start_time.strftime(date_time_fmt))
 
-ak_clf = ak.StructuredDataClassifier(max_trials=3)
-ak_clf.fit(X_train, y_train)
-ak_pred_y = ak_clf.predict(X_test)
+ak_clf = ak.StructuredDataClassifier(max_trials=1, loss='binary_crossentropy', objective="val_loss")
+ak_clf.fit(X_train, y_train, epochs=3, verbose=0, batch_size=16, seed=123)
 print(ak_clf.evaluate(X_test, y_test))
 
 # xgb_clf = xgb.XGBClassifier()
